366800bdd45350ce7075eefa491afe60ebe66257
Satoru Yokoyama <22640673+chao7150@users.noreply.github.com>
2020-10-11 14:48:04 +0900

    feat(src): 部屋割り

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import WebSocket from "ws";
 
 const wss = new WebSocket.Server({ port: 8080 });
-const clients = [] as Array<WebSocket>;
-wss.on("connection", (ws) => {
-  clients.push(ws);
+const clients: { [key: string]: Array<WebSocket> } = {};
+
+wss.on("connection", (ws, req) => {
+  if (!req.url) {
+    return undefined;
+  }
+  const url = new URL(req.url, "ws://hoge:8080");
+  const roomId = url.searchParams.get("roomid");
+  if (!roomId) {
+    return undefined;
+  }
+  if (roomId in clients) {
+    clients[roomId].push(ws);
+  } else {
+    clients[roomId] = [ws];
+  }
+
   ws.on("message", (message) => {
-    clients.forEach((client) => {
+    clients[roomId].forEach((client) => {
       if (client.readyState !== ws.OPEN) {
         return undefined;
       }
